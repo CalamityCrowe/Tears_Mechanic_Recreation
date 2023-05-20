cab56a3d45295b419e9cd36a63c6c4ca60fb892f
K.Thorpe <kieran123416@gmail.com>
2023-05-20 16:42:06 +0100

    Setup the queue system for the rewind mechanic

--- Source/Tears_Recreation/Rewindable.cpp
+++ Source/Tears_Recreation/Rewindable.cpp
@@ -6,16 +6,47 @@
 // Sets default values
 ARewindable::ARewindable()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh")); 
+	RootComponent = StaticMesh; 
+	m_RewindPositions = new TCircularQueue<pair<FVector, FRotator>>(29); 
+
+}
+
+void ARewindable::RewindObject(float lerpIntensity)
+{
+	if (m_isRewinding == true) 
+	{
+		
+	}
+}
+
+void ARewindable::GrabPosition()
+{
+	if (m_isRewinding == false) 
+	{
+		if (m_RewindPositions->IsFull() == false)
+		{
+			m_RewindPositions->Enqueue(pair<FVector, FRotator>(GetActorLocation(), GetActorRotation()));
+		}
+		else
+		{
+			m_RewindPositions->Dequeue();
+			m_RewindPositions->Enqueue(pair<FVector, FRotator>(GetActorLocation(), GetActorRotation()));
+		
+		}
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, FString::Printf(TEXT("%d"), m_RewindPositions->Count()));
+
+	}
 }
 
 // Called when the game starts or when spawned
 void ARewindable::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	GrabPosition();
 }
 
 // Called every frame
--- Source/Tears_Recreation/Rewindable.h
+++ Source/Tears_Recreation/Rewindable.h
@@ -5,30 +5,40 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include <utility>   
+#include "Containers/CircularQueue.h"
 #include "Rewindable.generated.h"
 
-using std::pair; 
+using std::pair;
 
 UCLASS()
 class TEARS_RECREATION_API ARewindable : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	ARewindable();
 
 	UFUNCTION(BlueprintCallable)
 		void RewindObject(float lerpIntensity);
+	void SetRewind(bool b) { m_isRewinding = b; }
+	bool GetRewind() { return m_isRewinding; }
+
+	UFUNCTION(BlueprintCallable)
+		void GrabPosition();
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	TQueue<pair<FVector, FRotator>> m_RewindPositions;
+	TCircularQueue<pair<FVector, FRotator>>* m_RewindPositions;
 	bool m_isRewinding;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mesh")
+		class UStaticMeshComponent* StaticMesh;
+
 
 };
