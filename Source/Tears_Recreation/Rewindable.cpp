c0940572731d3a8d76b0db7755f3f35c133fd854
K.Thorpe <kieran123416@gmail.com>
2023-05-24 15:23:20 +0100

    Scuffed Rewind Implemented

--- Source/Tears_Recreation/Rewindable.cpp
+++ Source/Tears_Recreation/Rewindable.cpp
@@ -10,52 +10,56 @@ ARewindable::ARewindable()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh")); 
-	RootComponent = StaticMesh; 
-	m_RewindPositions = new TCircularQueue<pair<FVector, FRotator>>(29); 
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = StaticMesh;
+	m_RewindPositions = new TArray<pair<FVector, FRotator>>();
 
 }
 
 void ARewindable::RewindObject(float lerpIntensity)
 {
-	if (m_isRewinding == true) 
+	if (m_isRewinding == true)
 	{
-		if (m_RewindPositions->IsEmpty()) 
+		if (m_RewindPositions->IsEmpty())
 		{
 			m_isRewinding = false;
+			
 		}
-		else 
+		else
 		{
-			if (nextPosition.first != m_RewindPositions->Peek()->first) 
+			if (abs(GetActorLocation().Length() - m_RewindPositions->Top().first.Length()) > 1)
 			{
-				nextPosition = *m_RewindPositions->Peek();
+				nextPosition = m_RewindPositions->Top();
 			}
 			else
 			{
-				m_RewindPositions->Dequeue(); 
+				m_RewindPositions->Pop();
+				GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Emptying")));
 			}
-			FVector tempPos = UKismetMathLibrary::VLerp(GetActorLocation(), nextPosition.first, lerpIntensity); 
-			FRotator tempRot = UKismetMathLibrary::RLerp(GetActorRotation(), nextPosition.second, lerpIntensity,false);
-			SetActorLocationAndRotation(tempPos, tempRot); 
+			FVector tempPos = UKismetMathLibrary::VLerp(GetActorLocation(), nextPosition.first, lerpIntensity);
+			FRotator tempRot = UKismetMathLibrary::RLerp(GetActorRotation(), nextPosition.second, lerpIntensity, false);
+			SetActorLocationAndRotation(tempPos, tempRot);
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("%d"), m_RewindPositions->Num()));
 		}
 	}
 }
 
 void ARewindable::GrabPosition()
 {
-	if (m_isRewinding == false) 
+	if (m_isRewinding == false)
 	{
-		if (m_RewindPositions->IsFull() == false)
+		if (m_RewindPositions->Num() < 30)
 		{
-			m_RewindPositions->Enqueue(pair<FVector, FRotator>(GetActorLocation(), GetActorRotation()));
+			m_RewindPositions->Push(pair<FVector, FRotator>(GetActorLocation(), GetActorRotation()));
 		}
 		else
 		{
-			m_RewindPositions->Dequeue();
-			m_RewindPositions->Enqueue(pair<FVector, FRotator>(GetActorLocation(), GetActorRotation()));
-		
+			m_RewindPositions->RemoveAt(0); 
+			m_RewindPositions->Push(pair<FVector, FRotator>(GetActorLocation(), GetActorRotation()));
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, FString::Printf(TEXT("AAAAAAAAAA")));
+
 		}
-		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, FString::Printf(TEXT("%d"), m_RewindPositions->Count()));
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, FString::Printf(TEXT("%d"), m_RewindPositions->Num()));
 
 	}
 }
@@ -69,13 +73,21 @@ void ARewindable::BeginPlay()
 
 void ARewindable::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
-	m_RewindPositions->Empty(); 
+	m_RewindPositions->Empty();
 }
 
 // Called every frame
 void ARewindable::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	if (m_isRewinding)
+	{
+		StaticMesh->SetSimulatePhysics(false);
+	}
+	else
+	{
+		StaticMesh->SetSimulatePhysics(true);
+	}
 
 }
 
--- Source/Tears_Recreation/Rewindable.h
+++ Source/Tears_Recreation/Rewindable.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Actor.h"
 #include <utility>   
 #include "Containers/CircularQueue.h"
+
 #include "Rewindable.generated.h"
 
 using std::pair;
@@ -22,10 +23,10 @@ public:
 	UFUNCTION(BlueprintCallable)
 		void RewindObject(float lerpIntensity);
 	UFUNCTION(BlueprintCallable)
-	void SetRewind(bool b) { m_isRewinding = b; }
+		void SetRewind(bool b) { m_isRewinding = b; }
 	bool GetRewind() { return m_isRewinding; }
 	UFUNCTION(BlueprintCallable)
-		int rewindCount() { return m_RewindPositions->Count() - 1; }
+		int rewindCount() { return m_RewindPositions->Num(); }
 	UFUNCTION(BlueprintCallable)
 		void GrabPosition();
 
@@ -36,11 +37,11 @@ protected:
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	TCircularQueue<pair<FVector, FRotator>>* m_RewindPositions;
+	TArray<pair<FVector, FRotator>>* m_RewindPositions;
 	bool m_isRewinding;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mesh")
 		class UStaticMeshComponent* StaticMesh;
-	pair<FVector, FRotator> nextPosition; 
+	pair<FVector, FRotator> nextPosition;
 
 };
